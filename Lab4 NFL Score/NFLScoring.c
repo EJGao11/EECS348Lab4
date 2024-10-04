ba24fbda8947d5971a36d87e8b46177658bfa88b
Ethan_Gao <e784g298@home.ku.edu>
2024-10-04 12:46:58 -0500

    Fixed NFL Scoring

--- Lab4 NFL Score/NFLScoring.c
+++ Lab4 NFL Score/NFLScoring.c
@@ -1,42 +1,93 @@
 #include <stdio.h>
-#include <string.h>
-void recScore(int scores[], int team[], int currentScore, int targetScore);
 
-int main()
+// Function to print the current combination
+void printCombination(int combination[], int count) 
 {
-    int targetScore = 0;
-    int currentScore = 0;
+    int TD2 =0;
+    int TD1 = 0;
+    int TD = 0;
+    int FG = 0;
+    int safty = 0;
 
-    int scores[] = {8,7,6,3,2};
-    int teamPlays[] ={0,0,0,0,0};
-    int currentPath = 0;
-    
-    printf("Enter NFL Score: ");
-    scanf("%d", &targetScore);
-    
-    recScore(scores, teamPlays, currentScore, targetScore);
-    return 0;
-}
-
-void recScore(int scores[], int team[], int currentScore, int targetScore)
-{
-    int copyTeam[strlen(team)*sizeof(int)];
-    memcpy(copyTeam,team,strlen(team)*sizeof(int));
-    for ( int i = 0; i < 5; i++)
+    for (int i = 0; i < count; i++) 
     {
-        if(currentScore + scores[i] <= targetScore)
+        switch( combination[i])
         {
-            copyTeam[i] +=  1;
-            recScore(scores, copyTeam, currentScore += scores[i], targetScore);
+            case 8:
+                TD2++;
+                break;
+
+            case 7:
+                TD1++;
+                break;
+            
+            case 6:
+                TD++;
+                break;
+            case 3:
+                FG++;
+                break;
+            case 2:
+                safty++;
+                break;
+                
+            default:
+                break;
         }
     }
-    if(currentScore == targetScore)
+    printf("TD 2pt Conversion: %d ", TD2);
+    printf(" TD Fieldgoal: %d ", TD1);
+    printf(" Towndown: %d ", TD);
+    printf(" Fieldgoal: %d ", FG);
+    printf(" Safty : %d ", safty);
+    printf("\n");
+}
+
+
+void findCombinations(int points[], int n, int sum, int combination[], int index) 
+{
+   
+    // Find vaild comb than sends to print
+    if (sum == 0) 
     {
-        printf("Twopoint Touchdown: %d", copyTeam[0]);
-        printf(" Fieldgoal Touchdown: %d", copyTeam[1]);
-        printf(" Touchdown: %d", copyTeam[2]);
-        printf(" Fieldgoal: %d", copyTeam[3]);
-        printf(" Safty: %d", copyTeam[4]);
-        printf("\n");
+        printCombination(combination, index);
+        return;
     }
+
+    // Over the point count or ran out of options
+    if (sum < 0 || n <= 0) 
+    {
+        return;
+    }
+
+    // Puts the play into comb
+    combination[index] = points[n - 1];
+   
+    // Inculde or exclude play
+    findCombinations(points, n, sum - points[n - 1], combination, index + 1);
+
+    findCombinations(points, n - 1, sum, combination, index);
+}
+
+// Sets up points and calls the recursive function
+int main() 
+{
+    int points[] = {2, 3, 6, 7, 8};
+
+    int targetScore = 0;
+    int combination[1000];
+
+    printf("Enter NFL Score: ");
+    scanf("%d", &targetScore);
+
+    if(targetScore == 0 || targetScore == 1)
+    {
+        printf("Invaild Score Enter something Higher than 2");
+    }
+    else
+    {
+        printf("Combinations of plays that sum to %d:\n", targetScore);
+        findCombinations(points, 5, targetScore, combination, 0);
+    }
+    return 0;
 }
